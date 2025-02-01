f8553e701a68750d41f80f40857a3db4e833f446
Plubu <60044928+Plubu@users.noreply.github.com>
2025-02-01 19:38:59 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
+// Import Mongoose for MongoDB object modeling
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-}, { timestamps: true });
+  username: { type: String, required: true, unique: true }, // To uniquely identify each user, provide a unique username
+  passwordHash: { type: String, required: true }, // Hashed password for security
+}, { timestamps: true }); // Automatic creation of createdAt and updatedAt timestamps
 
+// Export the User model based on the schema
 module.exports = mongoose.model("User", userSchema);
